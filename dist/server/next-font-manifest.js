4d03884c8b574bad36cefe72ad1e157ca163f492
Mark <96885623+Markukr@users.noreply.github.com>
2023-09-08 13:39:13 +0200

    change next.config, debug pictures, add nextjs.yml

---
+++ dist/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ivanuruskyi/Desktop/Code/plast-dk/plast_test_app/src/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
