b018bcb5ba00d84b785872ac3751f2e8cc1f3967
Mark <96885623+Markukr@users.noreply.github.com>
2023-09-11 13:49:39 +0200

    change darkMode3

--- src/app/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/app/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,13 +1,12 @@
 "use client"
 
 import React, { useContext } from "react";
-import styles from "@/app/components/DarkModeToggle/DarkModeToggle.module.css"
+import styles from './DarkModeToggle.module.css';
 import { ThemeContext } from "@/context/ThemeContext";
 
 
-
 const DarkModeToggle = () => {
-   const { toggle, mode } = useContext(ThemeContext)
+   const { toggle, mode } = useContext(ThemeContext);
    return (
       <div className={styles.container} onClick={toggle}>
          <div className={styles.icon}>🌙</div>
